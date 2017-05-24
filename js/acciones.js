0201b9df7349e3c6b683ab2ef36f13cc9719c6cc
solvazquez35 <solvazquez135@gmail.com>
2017-05-24 09:33:44 -0500

    4subida

--- js/acciones.js
+++ js/acciones.js
@@ -32,7 +32,7 @@ function getPosition(){
 		  'Longitude:'     +position.coords.longitude  +'\n'+
 		  'Altitude:'     +position.coords.altitude  +'\n'+
 		  'Accuracy:'     +position.coords.accuracy  +'\n'+
-		  'Altitude Accuracy:'     +position.coords.atitudeAccuracy  +'\n'+
+		  'Altitude Accuracy:'     +position.coords.altitudeAccuracy  +'\n'+
 	      'Heading:'     +position.coords.heading  +'\n'+
 		  'Speed:'     +position.coords.speed  +'\n'+
 		  'Timestamp:'     +position.timestamp  +'\n');
